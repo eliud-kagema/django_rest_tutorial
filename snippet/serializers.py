2e32b4100b01c44d1b453eb00e829746aba26875
Eluid Maina <Eluid.Maina@womensbank.co.zw>
2020-02-28 09:31:29 +0200

    adding owner to the serializer

--- snippet/serializers.py
+++ snippet/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 from snippet.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
-        owner = serializers.ReadOnlyField(source='owner.username')
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
 
 
 
